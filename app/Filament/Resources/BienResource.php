1fa474710ea50a2c5f47757fe87ab3a1a2afd4dc
Nathlo <thyalan.aurlence@gmail.com>
2024-06-27 19:14:13 -0400

    filled filament form and table for 'Biens'

---
+++ app/Filament/Resources/BienResource.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BienResource\Pages;
+use App\Filament\Resources\BienResource\RelationManagers;
+use App\Models\Bien;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BienResource extends Resource
+{
+    protected static ?string $model = Bien::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(60),
+                Forms\Components\TextInput::make('description'),
+                    
+                Forms\Components\TextInput::make('price'),
+                
+                Forms\Components\TextInput::make('city')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->columnSpan(2),
+                Tables\Columns\TextColumn::make('price')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('city')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBiens::route('/'),
+            'create' => Pages\CreateBien::route('/create'),
+            'edit' => Pages\EditBien::route('/{record}/edit'),
+        ];
+    }
+}
